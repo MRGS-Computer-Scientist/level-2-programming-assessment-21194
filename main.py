8caf33104f17252c9c4058e3c0afc3a69b5b8f40
21194 <aaran_kumar@hotmail.com>
2024-05-21 09:54:40 +1200

    Trying to figure out how to create and arrange grids

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("300x300")
+window.geometry("393x852")
 window.title("MRGSports")
+
+#navigationBar = Frame(window, bg="Orange", width=393,height=103)
+#navigationBar.grid(row=8, column=0)
+
+navigationBar = Frame(window, bg="Orange")
+navigationBar.grid(row=8, columnexpand=)
+
+homeButton = Button(navigationBar, width=46, height=46, bg="White", bd=10)
+homeButton.grid(row=8, column=0, padx=40, pady=20)
+
 
 window.mainloop()
