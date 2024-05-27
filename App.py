cea6b33d16112f9e79e0c8c667938e1e415b077e
21194 <aaran_kumar@hotmail.com>
2024-05-28 09:54:27 +1200

    Using grid or pack to place a button on a grid causes it to disappear

--- App.py
+++ App.py
@@ -4,34 +4,54 @@ from os import *
 
 class App():
     
+    
+    
     def __init__(self):
+        
         
         self.window = Tk()
         self.window.geometry("393x852")
         self.window.title("MRGSports")
 
-        self.logoFrame = Frame(self.window, background="Orange", width=w_width, height=100)
+        #logo frame
+
+        self.logoFrame = Frame(self.window, background="Orange", width=393, height=100)
+        
         self.logoFrame.grid(row=0, column=0)
 
-        self.contentFrame = Frame(self.window, background="White", width=w_width, height=600)
+        #content frame
+
+        self.contentFrame = Frame(self.window, background="White", width=393, height=600)
+        
         self.contentFrame.grid(row=1, column=0)
 
-        self.navigationBar = Frame(self.window, bg="Orange", width=w_width, height=152)
+        #navigation bar
+
+        self.navigationBar = Frame(self.window, bg="Orange", width=393, height=152)
+        
         self.navigationBar.grid(row=2, column=0)
 
-        #self.navigationButtons = Frame(self.navigationBar, bg="Orange", width=w_width, height=152)
-        #self.navigationButtons.grid(row=0,column=0)
+        #placing buttons on nav bar
 
-        self.homeButton = Button(self.navigationBar, text="Home", bg="White", )
-        self.homeButton.grid(row=0, column=0)
-
-        self.ResuButton = Button(self.navigationBar, text="Resu", bg="White", )
-        self.ResuButton.grid(row=0, column=1)
-
-        self.StatButton = Button(self.navigationBar, text="Stat", bg="White", )
-        self.StatButton.grid(row=0, column=2)
-
-        self.LeadButton = Button(self.navigationBar, text="Lead", bg="White", )
-        self.LeadButton.grid(row=0, column=3)
-
+        self.exampleButton = Button(self.navigationBar, text="Example Button")
+        
+        self.exampleButton.grid(row=0, column=0)
+        
+           
         self.window.mainloop()
+        
+        
+        
+        
+        
+        #self.homeButton = Button(self.navigationBar, text="Home", bg="White")
+        #self.homeButton.grid(row=0,column=0)
+
+        #self.ResuButton = Button(self.navigationBar, text="Resu", bg="White", )
+        #self.ResuButton.grid(row=0, column=1)
+
+        #self.StatButton = Button(self.navigationBar, text="Stat", bg="White", )
+        #self.StatButton.grid(row=0, column=2)
+
+        #self.LeadButton = Button(self.navigationBar, text="Lead", bg="White", )
+        #self.LeadButton.grid(row=0, column=3)
