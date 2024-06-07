2c4d0aa4423e54850f8050eebc501693db234740
21194 <aaran_kumar@hotmail.com>
2024-06-07 12:35:25 +1200

    Solved grid problem, another issue with centering widgets but started on page functionality

--- App.py
+++ App.py
@@ -20,7 +20,7 @@ class App():
         #logo text test
         
         self.logoText = Label(self.logoFrame, text="MRGSports")
-        self.logoText.grid(row=0, column=0)
+        self.logoText.grid(row=0, column=0, pady=40, rowspan=1)
         #self.logoText.pack()
 
         #content frame
@@ -28,7 +28,19 @@ class App():
         self.contentFrame = Frame(self.window, background="White", width=393, height=600)
         
         self.contentFrame.grid(row=1, column=0)
-
+        
+        #home page frame
+        
+        self.homePage = Frame(self.contentFrame, background="White", width=393, height=600)
+        
+        #stat page frame
+        
+        self.homePage = Frame(self.contentFrame, background="White", width=393, height=600)        
+        
+        #resu page frame
+        
+        self.homePage = Frame(self.contentFrame, background="White", width=393, height=600)
+        
         #navigation bar
 
         self.navigationBar = Frame(self.window, bg="Orange", width=393, height=152)
@@ -37,25 +49,22 @@ class App():
 
         #placing buttons on nav bar
 
-        self.exampleButton = Button(self.navigationBar, text="Example Button")
+        #self.exampleButton = Button(self.navigationBar, text="Example Button")
+        #self.exampleButton.grid(row=0,column=0)
         
-        self.exampleButton.grid(row=0,column=0)
+        self.homeButton = Button(self.navigationBar, text="Home", bg="White", width=7, command=self.goToHome)
+        self.homeButton.grid(row=0,column=0,)
+
+        self.ResuButton = Button(self.navigationBar, text="Resu", bg="White", width=7)
+        self.ResuButton.grid(row=0, column=1,padx=19,pady=65)
+
+        self.StatButton = Button(self.navigationBar, text="Stat", bg="White", width=7)
+        self.StatButton.grid(row=0, column=2,padx=19,pady=65)
+
+        self.LeadButton = Button(self.navigationBar, text="Lead", bg="White", width=7)
+        self.LeadButton.grid(row=0, column=3,padx=19,pady=65)
         
-           
         self.window.mainloop()
         
-        
-        
-        
-        
-        #self.homeButton = Button(self.navigationBar, text="Home", bg="White")
-        #self.homeButton.grid(row=0,column=0)
-
-        #self.ResuButton = Button(self.navigationBar, text="Resu", bg="White", )
-        #self.ResuButton.grid(row=0, column=1)
-
-        #self.StatButton = Button(self.navigationBar, text="Stat", bg="White", )
-        #self.StatButton.grid(row=0, column=2)
-
-        #self.LeadButton = Button(self.navigationBar, text="Lead", bg="White", )
-        #self.LeadButton.grid(row=0, column=3)
+    def goToHome(self):
+            
